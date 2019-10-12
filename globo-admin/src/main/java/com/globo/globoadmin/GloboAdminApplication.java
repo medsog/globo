8a0b947617c41db62cbcbab2f9f325bf6a1cd0aa
medsog <mohamaedsoghri@gmail.com>
2019-10-12 07:09:08 +0100

    [MS] first commit admi globo springboot

---
+++ globo-admin/src/main/java/com/globo/globoadmin/GloboAdminApplication.java
@@ -0,0 +1,13 @@
+package com.globo.globoadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GloboAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GloboAdminApplication.class, args);
+	}
+
+}
